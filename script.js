ff1b9f446e09b8573b32fed2a57f91428b82d911
fahim2k25 <fahim.program@gmail.com>
2025-04-11 19:39:34 +0600

    working on localstorage set item method

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 const saveList = () => {
-    const container = document.getElementById("list-container");
+
     const name = document.getElementById("name-input").value;
     const age = document.getElementById("age-input").value;
-    const li = document.createElement("li");
-    li.innerText = `${name} : ${age}`;
-    container.appendChild(li);
+    displayList(name, age);
+    setValueLocal(name, age);
     document.getElementById("name-input").value = ""
     document.getElementById("age-input").value = "";
+};
+
+const displayList = (productName, productQuantity) => {
+    const container = document.getElementById("list-container");
+    const li = document.createElement("li");
+    li.innerText = `${productName} : ${productQuantity}`;
+    container.appendChild(li);
+
+};
+const setValueLocal = (nam, quantity) => {
+    const newObj = { pencil: quantity };
+    localStorage.setItem("cart", JSON.stringify(newObj));
+
 };
